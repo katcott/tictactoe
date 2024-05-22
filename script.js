a4ea08bc7abd1462aa0bd2cfe7d0a4e094cf8685
kat <133708072+katcott@users.noreply.github.com>
2024-05-21 19:10:28 -0500

    added divs for gameboard and basic formatting

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+const Gameboard = (function() {
+    const board = [];
+
+})();
+
+function createUser(name){
+    const username = name;
+    return username;
+}
+
+
+function playGame(player1 = "Player 1", player2 = "Player 2"){
+    
+}
